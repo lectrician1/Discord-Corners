9cdf89e2ea90e9bd1ea5db0b61db39f1dfbb4e1c
Seth Deegan <jayandseth@gmail.com>
2018-05-22 17:18:13 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const http = require("http");
+const port = process.env.PORT;
+
+// Server keeps the bot with Uptime Robot pinging it
+const requestHandler = (request, response) => {
+  console.log(request.url);
+  response.end('Hello, I am Discord Corners!');
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'request') {
+    msg.reply('works');
+  }
+});
+
+client.login(process.env.token);
