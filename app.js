9a590b4564ce877d05e54ae34e3c87bc7f165da1
Seth Deegan <jayandseth@gmail.com>
2018-06-10 07:36:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ client.on('message', msg => {
                   const filter = m => m.content === 'approve' | m.content === 'disapprove';
                   msg.channel.awaitMessages(filter, { max: 1, time: 20000, errors: ['time'] })
                     .then(collected => {
-                      if (collected.values() == "approve") {
+                      if (collected.values().next().value == "approve") {
                         console.log('ok');
                         user.send('yaya');
                       }
