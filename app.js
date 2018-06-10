68df50c20eb9755323a4a3babdc4d483c3869175
Seth Deegan <jayandseth@gmail.com>
2018-06-10 07:32:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -55,8 +55,10 @@ client.on('message', msg => {
                   const filter = m => m.content === 'approve' | m.content === 'disapprove';
                   msg.channel.awaitMessages(filter, { max: 1, time: 20000, errors: ['time'] })
                     .then(collected => {
-                      console.log(collected.values());
-                      user.send('yaya');
+                      if (collected.values() == "approve") {
+                        console.log('ok');
+                        user.send('yaya');
+                      }
                       console.log('7 received');
                     })
                     .catch(collected => console.log('lectrician1 never responded'));
