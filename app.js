cc3d0e9f67b3c5e4946969c4dc0587757a0f4fe8
Seth Deegan <jayandseth@gmail.com>
2018-06-14 12:46:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -28,32 +28,27 @@ client.on('ready', () => {
 client.on('message', msg => {
   var msgSplit = msg.content.split('.');
   if (msgSplit[0] === 'request') {
-    console.log('1 received');
     if (msgSplit[1] === 'partner') {
-      console.log('2 received');
       if (msgSplit[2].startsWith('(') & msgSplit[2].endsWith(')')) {
-        console.log('3 received');
         var msgSplit_1 = msgSplit[2]
         msgSplit_1.slice(1,-1);
         var msgSplit2 = msgSplit_1.split(', ');
         var msgSplit3 = [];
         for (i = 0; i < msgSplit2.length; i++) {
-          console.log('looping' + i);
           var temp = msgSplit2[i].split(': ');
           msgSplit3.push(temp[0], temp[1]);
         }
         console.log(msgSplit3);
         if (msgSplit3[0] === '(invite') {
-          console.log('4 received');
           if (msgSplit3[1].includes('discordgg')) {
-            console.log('5 received');
             if (msgSplit3[2] === 'desc') {
               console.log('6 received');
-              var formated = `Invite: ${msgSplit3[1]} \n Owner: blah \n Description: ${msgSplit3[3]}`
-              client.fetchUser(msg.author.id).then(user => {
-                user.send(formated).then(msg => {
+              msg.author.createDM()
+                .then(DMchannel => {
+                  var formated = `Invite: ${msgSplit3[1]} \n Owner: blah \n Description: ${msgSplit3[3]}`
+                  DMchannel.send(formated);
                   const filter = m => m.content === 'approve' | m.content === 'disapprove';
-                  msg.channel.awaitMessages(filter, { max: 1, time: 20000, errors: ['time'] })
+                  DMchannel.awaitMessages(filter, { max: 1, time: 20000, errors: ['time'] })
                     .then(collected => {
                       console.log(`Size: ${collected.size}`);
                       console.log(collected.values());
@@ -69,8 +64,7 @@ client.on('message', msg => {
                     })
                     .catch(collected => console.log('lectrician1 never responded'));
                 });
-              });
-            }
+            } 
           }
         }
       }
