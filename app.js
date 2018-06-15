2d4ff513690a2466b4dfadd3ffbd69f498bd49b3
Seth Deegan <jayandseth@gmail.com>
2018-06-15 13:04:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ client.on('message', msg => {
         if (msgSplit3[0] === '(invite') {
           if (msgSplit3[1].includes('discord.gg/')) {
             if (msgSplit3[2] === 'desc') {
-              client.fetchinvite('https://discord.gg/7S94fr2')
+              client.fetchInvite('https://discord.gg/7S94fr2')
                 .then(invite => {
                   console.log(invite.guild.members);
                 });
