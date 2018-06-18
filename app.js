4ba5b1cd6855914eec12e2d498f66dd7bcff3537
Seth Deegan <jayandseth@gmail.com>
2018-06-18 11:29:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.fetchInvite('https://discord.gg/7S94fr2')
     .then(invite => {
-      console.log(invite.guild.members.find('username', 'lectrician1'));
+      console.log(invite.guild.members);
     });
 });
 
