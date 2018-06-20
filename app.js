2bf8815b5c2c9ff2e8c3ac334c2229b48d43e24b
Seth Deegan <jayandseth@gmail.com>
2018-06-19 19:00:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,8 @@ client.on('message', msg => {
         if (parameterValues[0] === 'invite') {
           if (parameterValues[1].includes('discord.gg/')) {
             if (parameterValues[2] === 'desc' || parameterValues[2] === ' desc') {
-              if (parameterValues.length === 4 & parameterValues[3].match('/[A-z|0-9]+/g').length > 0) {
+              var parameterValuesMatch = parameterValues[3].match('/[A-z|0-9]+/g');
+              if (parameterValues.length === 4 & parameterValuesMatch.length > 0) {
                 client.fetchInvite('https://discord.gg/7S94fr2')
                   .then(invite => {
                     var requestResult = false;
